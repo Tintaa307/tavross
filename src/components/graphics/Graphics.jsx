c4971426b833380cd689c8b1f8ea8a7992a4cb3f
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2022-09-18 02:07:31 -0300

    added graphic styles

--- src/components/graphics/Graphics.jsx
+++ src/components/graphics/Graphics.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from "react"
 import "./graphics.css"
-import { Bar } from "react-chartjs-2"
+import { Line } from "react-chartjs-2"
 import Chart from "chart.js/auto"
 
 const Graphics = ({ chartData }) => {
@@ -11,6 +11,21 @@ const Graphics = ({ chartData }) => {
 
   const handleChange = (e) => {
     setTitle(e.target.value)
+  }
+
+  //change design of chart
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+        labels: {
+          usePointStyle: true,
+          pointStyle: "circle",
+        },
+      },
+    },
   }
 
   return (
@@ -32,8 +47,8 @@ const Graphics = ({ chartData }) => {
       <div className="container-graphics-info">
         <div className="graphics">
           <h3>{title}</h3>
-          <div style={{ color: "#fff" }} className="graphic">
-            <Bar data={chartData}></Bar>
+          <div className="graphic">
+            <Line options={options} data={chartData}></Line>
           </div>
         </div>
       </div>
