3a54141fd42db90b4b4c264a5666aa28699e19e5
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2022-09-29 15:36:31 -0300

    modify create rutine

--- src/components/rutines/CreateRutine.jsx
+++ src/components/rutines/CreateRutine.jsx
@@ -1,7 +1,21 @@
-import React from "react"
+import React, { useRef, useState } from "react"
 import "./createRutine.css"
 
 const CreateRutine = () => {
+  const [change, setChange] = useState("")
+  const nameRef = useRef(null)
+  const typeRef = useRef(null)
+  const [res, setRes] = useState("")
+
+  const handleChange = () => {
+    if (nameRef.current.value === "" || typeRef.current.value === "") {
+      setRes("Todos los campos son requeridos")
+    } else {
+      change === "" ? setChange("change") : setChange("")
+      setRes("")
+    }
+  }
+
   return (
     <div className="container-create-rutine">
       <h1>Crea Tu Rutina</h1>
@@ -10,23 +24,45 @@ const CreateRutine = () => {
           <div className="container-headers">
             <div className="item">
               <h5>Paso 1</h5>
-              <div className="circle">
-                1 <i class="ri-check-line"></i>
+              <div className={["circle", change].join(" ")}>
+                <span>1</span>{" "}
+                <i class={["ri-check-line", change].join(" ")}></i>
               </div>
             </div>
             <div className="item">
               <h5>Paso 2</h5>
               <div className="circle">
-                2 <i class="ri-check-line"></i>
+                <span>2</span> <i class="ri-check-line"></i>
               </div>
             </div>
             <div className="item">
               <h5>Fin</h5>
               <div className="circle">
-                3 <i class="ri-check-line"></i>
+                <span>3</span> <i class="ri-check-line"></i>
               </div>
             </div>
           </div>
+          <form className="form">
+            <div className={["steps", change].join(" ")}>
+              <div className="item">
+                <h4>Nombre de la rutina</h4>
+                <input ref={nameRef} type="text" />
+                <div className="message">{res}</div>
+              </div>
+              <div className="item">
+                <h4>Fin de la rutina</h4>
+                <select ref={typeRef} className="select">
+                  <option value="salud">Salud</option>
+                  <option value="fuerza">Fuerza</option>
+                  <option value="cardio">Cardio</option>
+                </select>
+              </div>
+              <div className="btn-sig" onClick={handleChange}>
+                Siguiente
+              </div>
+            </div>
+            {/* <button type="submit"></button> */}
+          </form>
         </div>
         <form className="form"></form>
       </div>
