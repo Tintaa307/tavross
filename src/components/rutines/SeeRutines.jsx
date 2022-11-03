821014f4726034b6ab35f6c24a880e5c525df8df
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2022-11-03 20:00:48 -0300

    create see rutines

---
+++ src/components/rutines/SeeRutines.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./rutines.css"
+
+const SeeRutines = () => {
+  return (
+    <section className="section-see-rutines">
+      <div className="container-content-see-rutines"></div>
+    </section>
+  )
+}
+
+export default SeeRutines
